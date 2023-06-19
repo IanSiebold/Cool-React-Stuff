48a0ad639e5701d066e50d7efb33d5b9b16782d9
Ian Siebold <35971993+IanSiebold@users.noreply.github.com>
2023-06-19 13:08:23 -0500

    Create typing.js

---
+++ TypingHeader/typing.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default function Typing({ text }) {
+    const wrapperStyle = {
+        height: '100vh',
+        display: 'grid',
+        placeItems: 'center',
+    };
+
+    const typingStyle = {
+        width: `${text.length}ch`,
+        animation: `typing 2s steps(${text.length}), blink .5s step-end infinite alternate`,
+        whiteSpace:'nowrap',
+        overflow:'hidden',
+        borderRight: '3px solid',
+        fontFamily: 'monospace',
+        fontSize: '2em',
+    };
+
+    const keyframes =  `
+        @keyframes typing { 
+            from {
+                width: 0;
+            }
+        }
+
+        @keyframes blink {
+            50% {
+                border-color: transparent;
+            }
+        }
+        `;
+    return (
+        <div style={wrapperStyle}>
+            <style>{keyframes}</style>
+            <div style={typingStyle}>
+                {text}
+            </div>
+        </div>
+    )                                                          
+}
